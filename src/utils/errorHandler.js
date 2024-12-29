1ce6eab36af0e9885e958f5f14f24aa7a47ac7fa
Aftab <ftab7k@gmail.com>
2024-12-29 14:26:54 +0530

    ok

--- src/utils/errorHandler.js
+++
@@ -1,6 +0,0 @@
-const handleError = (error) => {
-  console.error('Bot Error:', error.message);
-  // Don't exit process on errors, just log them
-};
-
-module.exports = { handleError };
--- src/utils/responseTemplates.js
+++
@@ -1,7 +0,0 @@
-const { SOURCE_CODE_URL } = require('../config');
-
-const templates = {
-  sourceCode: `🤖 Here's my source code: ${SOURCE_CODE_URL}\n\nFeel free to check it out! ⭐️`
-};
-
-module.exports = templates;
